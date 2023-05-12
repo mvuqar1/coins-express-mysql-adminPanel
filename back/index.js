79a64f7e80998f3283e5cbc87c6460905c3a85fd
mvuqar1 <vuqar.5100@mail.ru>
2023-05-13 03:56:08 +0400

    vercel

--- back/index.js
+++ back/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
@@ -13,12 +14,20 @@ const PORT = 3001;
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "brehhu1gyxjrri72pirs-mysql.services.clever-cloud.com",
-  port: 3306, // опционально, если вы используете порт отличный от стандартного
-  user: "u3yhc9h0zeb5fgf0",
-  password: "gncznyTaXLVzThzHph6P",
-  database: "brehhu1gyxjrri72pirs"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
+
+// const connection = mysql.createConnection({
+//   host: "brehhu1gyxjrri72pirs-mysql.services.clever-cloud.com",
+//   port: 3306, // опционально, если вы используете порт отличный от стандартного
+//   user: "u3yhc9h0zeb5fgf0",
+//   password: "gncznyTaXLVzThzHph6P",
+//   database: "brehhu1gyxjrri72pirs"
+// });
 
 
 // const connection = mysql.createConnection({
