3e37a0177991d7d91b4bb74d424e7bbe9299a118
mvuqar1 <vuqar.5100@mail.ru>
2023-05-13 02:57:31 +0400

    vercel

--- back/index.js
+++ back/index.js
@@ -22,7 +22,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB-USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
   waitForConnections: true,
