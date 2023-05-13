c5ad21a36bc67f1e112b51ceb994ff7b3cf0ee24
mvuqar1 <vuqar.5100@mail.ru>
2023-05-13 06:22:50 +0400

    vercel

--- back/index.js
+++ back/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
 
-app.use(cors({
-  origin:"*"
+app.use(
+  cors({
+  origin:"*",
 }));
 
 // app.use(express.json());
