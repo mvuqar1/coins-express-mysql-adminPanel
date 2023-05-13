785b1b831d8b85cb7d3e929802d861378040ddb5
mvuqar1 <vuqar.5100@mail.ru>
2023-05-13 06:31:07 +0400

    vercel

--- back/index.js
+++ back/index.js
@@ -348,7 +348,7 @@ app.get("/options", (req, res) => {
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
