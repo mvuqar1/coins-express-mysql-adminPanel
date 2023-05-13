5fea4559d2b4545c1b60bd8ff2fc3b5ba5327372
mvuqar1 <vuqar.5100@mail.ru>
2023-05-13 17:59:18 +0400

    vercel

--- back/index.js
+++ back/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 const PORT = process.env.PORT || 3001;
 
